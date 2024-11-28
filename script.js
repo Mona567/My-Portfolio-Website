c060bd4e85c8932c1c4f30d81dee8b1eb467f02c
Mona567 <monajaiswal1192000@gmail.com>
2024-11-29 02:01:03 +0530

    change in code of script.js

--- script.js
+++ script.js
@@ -10,20 +10,4 @@ function rotateSkills() {
     setTimeout(rotateSkills, 2000);
 }
 
-function showSection(sectionId) {
-    // Hide all sections
-    const sections = document.querySelectorAll('.content-section');
-    sections.forEach(section => section.style.display = 'none');
-
-    // Show the selected section
-    const selectedSection = document.getElementById(sectionId);
-    if (selectedSection) {
-        selectedSection.style.display = 'block';
-    }
-}
-
-// Show the About section by default
-window.onload = () => {
-    showSection('about');
-    rotateSkills();
-};
+window.onload = rotateSkills;
